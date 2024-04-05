c45e01ec0f11dc77f89403489aaa39fc5d7bc550
Costantin777 <145683428+Costantin777@users.noreply.github.com>
2024-04-05 12:00:41 +0300

    new

---
+++ src/components/App/App.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+// import css from './App.module.css';
+import Description from "../Description/Description";
+import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
+import Notification from "../Notification/Notification";
+
+export default function App() {
+  const [state, setState] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(state));
+  }, [state]);
+
+  const updateFeedback = (feedbackType) => {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        [feedbackType]: prevState[feedbackType] + 1,
+      };
+    });
+  };
+
+  const resetFeedback = () => {
+    setState({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = state.good + state.neutral + state.bad;
+  const positiveFeedbacks = Math.round((state.good / totalFeedback) * 100);
+
+  return (
+    <>
+      <Description />
+      <Options
+        clickHandler={updateFeedback}
+        reset={resetFeedback}
+        total={totalFeedback}
+      />
+      <br />
+      {totalFeedback > 0 ? (
+        <Feedback
+          state={state}
+          total={totalFeedback}
+          positive={positiveFeedbacks}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+}
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,24 @@
+import css from "./Options.module.css";
+
+export default function Options({ clickHandler, reset, total }) {
+  return (
+    <div>
+      <button className={css.button} onClick={() => clickHandler("good")}>
+        Good
+      </button>
+      <button className={css.button} onClick={() => clickHandler("neutral")}>
+        Neutral
+      </button>
+      <button className={css.button} onClick={() => clickHandler("bad")}>
+        Bad
+      </button>
+      {total > 0 ? (
+        <button className={css.button} onClick={reset}>
+          Reset
+        </button>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
